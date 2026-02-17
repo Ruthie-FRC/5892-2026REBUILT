86496fef5cc4a3eff4b3ada0259ee0fb0bcad9aa
Michael <117385299+mstatistics@users.noreply.github.com>
2026-02-16 18:27:58 -0600

    Remove Hood from GenericPositionMechanismSubsystem I'll keep it around for now, but it isn't really worth it

--- src/main/java/frc/robot/subsystems/shooter/Hood.java
+++ src/main/java/frc/robot/subsystems/shooter/Hood.java
@@ -10,6 +10,8 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.SlotConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -20,38 +22,60 @@ import edu.wpi.first.units.measure.MutAngle;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.RobotState;
 import frc.robot.util.FieldConstants.LinesHorizontal;
 import frc.robot.util.FieldConstants.LinesVertical;
-import frc.robot.util.GenericPositionMechanismSubsystem;
 import frc.robot.util.LoggedDIO.LoggedDIO;
 import frc.robot.util.LoggedTalon.TalonFX.LoggedTalonFX;
 import frc.robot.util.LoggedTunableMeasure;
 import frc.robot.util.LoggedTunableNumber;
+import frc.robot.util.MechanismUtil;
+import java.util.function.Supplier;
+import lombok.Getter;
+import lombok.Setter;
 import org.littletonrobotics.junction.AutoLogOutput;
 
-public class Hood extends GenericPositionMechanismSubsystem {
+public class Hood extends SubsystemBase {
+  /* Hardware */
+  private final LoggedTalonFX motor;
+  private final LoggedDIO reverseLimit;
+  private final LoggedDIO forwardLimit;
+
+  /* Movement Constants */
   private final LoggedTunableMeasure<MutAngle> stowPosition =
       new LoggedTunableMeasure<>("Hood/StowAngle", Degrees.mutable(15));
-
   public static LoggedTunableNumber stowTrenchGapOffset =
       new LoggedTunableNumber("Hood/stowTrenchGapOffset", 0, "m");
+  private final LoggedTunableMeasure<MutAngle> tolerance =
+      new LoggedTunableMeasure<>("Hood/Tolerance", Degrees.mutable(5));
+  /* Homing */
+  private final LoggedTunableNumber homingVoltage =
+      new LoggedTunableNumber("Hood/Homing/Voltage", 4, "v");
+  private final LoggedTunableNumber homingConfirmationVoltage =
+      new LoggedTunableNumber("Hood/Homing/ConfirmVoltage", 4, "v");
+  private final LoggedTunableMeasure<MutAngle> homingSwitchPosition =
+      new LoggedTunableMeasure<>("Hood/Homing/homePosition", Rotations.mutable(0));
+  private final LoggedTunableMeasure<MutAngle> homingConfirmPosition =
+      new LoggedTunableMeasure<>("Hood/Homing/homePosition", Rotations.mutable(0.1));
 
-  @AutoLogOutput(key = "Hood/TrenchAreas")
-  public static Rectangle2d[] trenchAreas = new Rectangle2d[4];
+  /* State */
+  @AutoLogOutput private Rotation2d targetPosition = Rotation2d.kZero;
+  @AutoLogOutput private boolean positionControl = false;
+  @AutoLogOutput @Setter private boolean homed = false;
+  @AutoLogOutput @Getter private boolean atSetpoint = false;
+  @AutoLogOutput private Rectangle2d[] trenchAreas = new Rectangle2d[4];
+
+  /* Control  Requests*/
+  private final NeutralOut neutralControl = new NeutralOut();
+  private final MotionMagicVoltage mmControl = new MotionMagicVoltage(targetPosition.getMeasure());
 
   public Hood(LoggedTalonFX motor, LoggedDIO reverseLimit, LoggedDIO forwardLimit) {
-    super(
-        "Hood",
-        motor,
-        reverseLimit,
-        forwardLimit,
-        new LoggedTunableNumber("Hood/Homing/Voltage", 4, "v"),
-        new LoggedTunableNumber("Hood/Homing/ConfirmVoltage", 4, "v"),
-        new LoggedTunableMeasure<>("Hood/Homing/homePosition", Rotations.mutable(0))::get,
-        new LoggedTunableMeasure<>("Hood/Homing/homePosition", Rotations.mutable(0.1))::get,
-        new LoggedTunableMeasure<>("Hood/Tolerance", Degrees.mutable(5))::get);
+    this.motor = motor;
+    this.reverseLimit = reverseLimit;
+    this.forwardLimit = forwardLimit;
     updateTrenchAreas();
     var config =
         new TalonFXConfiguration()
@@ -91,6 +115,63 @@ public class Hood extends GenericPositionMechanismSubsystem {
         .withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
   }
 
+  public Command homingCommand() {
+    return MechanismUtil.buildHomingCommand(
+            motor,
+            reverseLimit,
+            this,
+            homingVoltage,
+            false,
+            homingSwitchPosition::get,
+            homingConfirmationVoltage,
+            homingConfirmPosition::get)
+        .beforeStarting(() -> positionControl = false)
+        .finallyDo(
+            (i) -> {
+              if (!i) {
+                setHomed(true);
+              }
+            });
+  }
+
+  /**
+   * A command that requests the turret to move to a position. The command completes imminently,
+   * without waiting for a tolerance to be achieved.
+   *
+   * @param position a supplier of the target position
+   * @return the command
+   */
+  public Command requestPosition(Supplier<Rotation2d> position) {
+    return runOnce(
+        () -> {
+          requestPosition(position.get());
+        });
+  }
+
+  public void requestPosition(Rotation2d position) {
+    targetPosition = position;
+    positionControl = true;
+    setControl();
+  }
+
+  /**
+   * A command that commands the Turret to move to a position. This command ends when the setpoint
+   * is archived
+   *
+   * @param position a supplier of the target position
+   * @return the command
+   */
+  public Command gotoPosition(Supplier<Rotation2d> position) {
+    // I really shouldn't but by creating a functional command I don't create 5 extra objects by
+    // separating this out.
+    return new FunctionalCommand(
+        () -> requestPosition(position.get()),
+        () -> {}, // Nothing to do periodically. Motion is controlled in the periodic function
+        (i) -> {},
+        () -> atSetpoint,
+        this);
+  }
+
   @AutoLogOutput(key = "Hood/ShouldStow")
   public boolean shouldStow() {
     final Pose2d pose = RobotState.getInstance().getRobotPosition();
@@ -100,12 +181,6 @@ public class Hood extends GenericPositionMechanismSubsystem {
       }
     }
     return false;
-  }
-
-  @Override
-  protected void periodicUser() {
-    ShotCalculator.getInstance().clearCache();
-    LoggedTunableNumber.ifChanged(this, (value) -> this.updateTrenchAreas(), stowTrenchGapOffset);
   }
 
   private void updateTrenchAreas() {
@@ -135,5 +210,30 @@ public class Hood extends GenericPositionMechanismSubsystem {
               new Translation2d(
                   LinesVertical.neutralZoneFar + offset, LinesHorizontal.rightTrenchOpenEnd))
         };
+  }
+
+  @Override
+  public final void periodic() {
+    motor.periodic();
+    reverseLimit.periodic();
+    forwardLimit.periodic();
+    atSetpoint = motor.atSetpoint(targetPosition.getMeasure(), tolerance.get());
+
+    ShotCalculator.getInstance().clearCache();
+    LoggedTunableNumber.ifChanged(this, (value) -> this.updateTrenchAreas(), stowTrenchGapOffset);
+
+    setControl();
+  }
+
+  private void setControl() {
+    if (positionControl) {
+      motor.setControl(
+          mmControl
+              .withPosition(targetPosition.getMeasure())
+              .withLimitReverseMotion(reverseLimit.get())
+              .withLimitForwardMotion(forwardLimit.get()));
+    } else {
+      motor.setControl(neutralControl);
+    }
   }
 }
