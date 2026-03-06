96da245d2d5f243adcc9bc69e928bf4b384b70d2
Michael <117385299+mstatistics@users.noreply.github.com>
2026-03-06 22:38:43 +0000

    stuff

---
+++ src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,76 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.PathPoint;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.indexer.Indexer;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.ShotCalculator.Goal;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.simple.parser.ParseException;
+import org.littletonrobotics.junction.Logger;
+
+/** Add your docs here. */
+public class Autos {
+
+  public static Command leftCenterAuto(
+      Intake intake, Indexer indexer, Shooter shooter) {
+    try {
+      final ArrayList<PathPoint> points;
+      if (Constants.currentMode == Constants.Mode.SIM) {
+        points = new ArrayList<>();
+      } else {
+        points = null;
+      }
+      // Boilerplate is over. Now do the actual logic
+      final Command auto =
+          AutoBuilder.followPath(loadPath("Left_Path_Start", points)).raceWith(intake.intakeSequence(),ShootCommands.shoot(indexer, shooter,Goal.LEFT)).andThen(
+
+          );
+      // More boilerplate
+      if (Constants.currentMode == Constants.Mode.SIM) {
+        Logger.recordOutput(
+            "Autos/Left Auto", points.stream().map(m -> m.position).toArray(Translation2d[]::new));
+      }
+      return auto;
+    } catch (Exception e) {
+      @SuppressWarnings("resource")
+      Alert alert = new Alert("Failed to load leftCenter Auto", AlertType.kError);
+      alert.set(true);
+      DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
+      return Commands.none();
+    }
+  }
+
+  public static PathPlannerPath loadPath(String name, List<PathPoint> points)
+      throws IOException, ParseException {
+    var path = PathPlannerPath.fromPathFile(name);
+    if (points != null) {
+      points.addAll(path.getAllPathPoints());
+    }
+    return path;
+  }
+
+  private static Command loadLogFollow(String name, List<PathPoint> points)
+      throws IOException, ParseException {
+    return AutoBuilder.followPath(loadPath(name, points))
+        .withName(name);
+  }
+}
--- src/main/java/frc/robot/commands/ShootCommands.java
+++ src/main/java/frc/robot/commands/ShootCommands.java
@@ -2,13 +2,22 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.RobotState;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.ShotCalculator.Goal;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ShootCommands {
+  public static Command shoot(Indexer indexer, Shooter shooter,Goal goal) {
+    return Commands.race(
+        indexer.outtake(),
+        shooter.getFlywheel().aimCommand(),
+        shooter.getHood().aimCommand(),
+        shooter.getTurret().aimCommand()).beforeStarting(()->RobotState.getInstance().setGoal(goal));
+  }
   public static Command shoot(Indexer indexer, Shooter shooter) {
     return Commands.race(
         indexer.outtake(),
